751d20b17ff59f80131ab311ce22575dc88294b8
Doruk Sinayuc <dsinayuc@gmail.com>
2016-02-11 20:49:13 +0200

    Initial Commit

---
+++ arrayTest2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  arrayTest2
+//
+//  Created by Doruk Sınayuç on 11/02/16.
+//  Copyright © 2016 Doruk Sınayuç. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
