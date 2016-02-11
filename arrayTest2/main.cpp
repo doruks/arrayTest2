14b5a61e78f6aeead3abbd5606e917747b1e610f
Doruk Sınayuç <doruksinayuc@Doruks-MacBook.local>
2016-02-11 22:01:34 +0200

    sortedArray is added

--- arrayTest2/main.cpp
+++ arrayTest2/main.cpp
@@ -12,30 +12,56 @@
 using namespace std;
 
 int getRandomNumber();
+int getSmallRandomNumber();
 
 int lowestElement(int array[], int noOfElement);
 
 int main(int argc, const char * argv[]) {
     int randomNumber[10];
+    int sortedNumber[10];
+    int lowElement = 0;
     
     for (int i=0; i<10; i++)
     {
-        sleep(1);
+        sleep(getSmallRandomNumber());
         randomNumber[i]=getRandomNumber();
         cout <<i<<". random number is: "<<randomNumber[i]<<endl;
         
     }
+    
+    
+    
+    cout << "lowest element is: " << lowElement << endl;
+    
+    
+    
+    for (int i=0; i<10; i++)
+    {
+        cout << i << ". random number is: " << randomNumber[i] << endl;
+        lowElement = lowestElement(randomNumber, 10);
+        sortedNumber[i] = lowElement;
+        cout << i << ". sorted number is: " << sortedNumber[i] << endl;
+    }
+    
+    
+    
     
     return 0;
 }
 
 int lowestElement(int array[], int noOfElement) {
     int lowest = 101;
+    int lowestIndex = 0;
     for (int i=0; i<noOfElement; i++) {
         if (array[i]<lowest) {
             lowest = array[i];
+            lowestIndex = i;
         }
     }
+    
+    
+    
+    array[lowestIndex] = 101;
     
     return lowest;
 }
@@ -47,6 +73,16 @@ int getRandomNumber()
     
     srand((unsigned int) time(NULL));
     aNumber =  (rand()%100)+1;
+    return aNumber;
+    
+}
+
+int getSmallRandomNumber()
+{
+    int aNumber = 0;
+    
+    srand((unsigned int) time(NULL));
+    aNumber =  (rand()%3)+1;
     return aNumber;
     
 }
