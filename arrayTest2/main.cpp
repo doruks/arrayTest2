1631affbd9b6ac7225b0fa75c18db532fb1e89a0
Doruk Sınayuç <doruksinayuc@Doruks-MacBook.local>
2016-02-13 14:58:23 +0200

    new version where you pick if you want to pic high to low or low to high

--- arrayTest2/main.cpp
+++ arrayTest2/main.cpp
@@ -9,17 +9,30 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 int getRandomNumber();
 
 int lowestElement(int array[], int noOfElement);
+int highestElement(int array[], int noOfElement);
 
 int main(int argc, const char * argv[]) {
     srand((unsigned int) time(NULL));
     int randomNumber[10];
-    int sortedNumber[10];
+    int sortedNumberLowToHigh[10];
+    int sortedNumberHighToLow[10];
     int lowElement = 0;
+    int highElement = 0;
+    string whatToDo;
+    
+    
+    cout<<"please enter the way you want to sort your number. "<<endl;
+    cout<<"You may choose from higherToLower or lowerToHigher. "<<endl;
+    cin>>whatToDo;
+    
+    if (whatToDo == "lowerToHigher") {
+        
     
     for (int i=0; i<10; i++)
     {
@@ -34,17 +47,42 @@ int main(int argc, const char * argv[]) {
     for (int i=0; i<10; i++)
     {
         lowElement = lowestElement(randomNumber, 10);
-        sortedNumber[i] = lowElement;
+        sortedNumberLowToHigh[i] = lowElement;
         
     }
 
     
     for (int i=0; i<10; i++) {
-        cout << i << ". sorted number is: " << sortedNumber[i] << endl;
+        cout << i << ". sorted number is: " << sortedNumberLowToHigh[i] << endl;
     }
     
     
+    }
     
+    else if(whatToDo == "higherToLower")
+    {
+        for (int i =0; i<10; i++)
+        {
+            randomNumber[i]=getRandomNumber();
+            cout <<i<<". random number is: "<<randomNumber[i]<<endl;
+        }
+        
+        
+        cout << "==========================="<<endl;
+        
+        
+        for (int i=0; i<10; i++)
+        {
+            highElement = highestElement(randomNumber, 10);
+            sortedNumberHighToLow[i] = highElement;
+        }
+        
+        for (int i=0; i<10; i++)
+        {
+            cout << ". sorted number is: " << sortedNumberHighToLow[i] << endl;
+        }
+
+    }
     
     return 0;
 }
@@ -66,6 +104,23 @@ int lowestElement(int array[], int noOfElement) {
     return lowest;
 }
 
+int highestElement(int array[], int noOfElement){
+    
+    int highest =1;
+    int highestIndex = 0;
+    for(int i=0; i<noOfElement; i++) {
+        
+        if(array[i]>highest)
+        {
+            highest = array[i];
+            highestIndex = i;
+        }
+    }
+    
+    array [highestIndex] = 1;
+    
+    return highest;
+}
 
 int getRandomNumber()
 {
